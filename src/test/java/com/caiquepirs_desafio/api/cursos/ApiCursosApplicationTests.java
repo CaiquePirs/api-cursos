df069e13ae4ef1425cd3c8298aefb9d0a8ba5128
023.959532 <c.pires@ba.estudante.senai.br>
2025-04-14 20:56:06 -0300

    Primeiro commit

---
+++ src/test/java/com/caiquepirs_desafio/api/cursos/ApiCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiquepirs_desafio.api.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
