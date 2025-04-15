9b54fd5f9dbe47ed2ded96546d93b396ca152c0e
023.959532 <c.pires@ba.estudante.senai.br>
2025-04-15 20:40:23 -0300

    Criando a classe controladora

---
+++ src/main/java/com/caiquepirs_desafio/api/cursos/controller/CursoController.java
@@ -0,0 +1,19 @@
+package com.caiquepirs_desafio.api.cursos.controller;
+
+import com.caiquepirs_desafio.api.cursos.model.CursoEntity;
+import com.caiquepirs_desafio.api.cursos.service.CursoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/course")
+public class CursoController {
+
+    @Autowired
+    private CursoService cursoService;
+    
+
+}
